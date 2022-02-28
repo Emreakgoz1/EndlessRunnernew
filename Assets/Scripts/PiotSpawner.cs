cf74c1dc1311f716a7e04604ea249d408c8f860e
Emre Akgöz <baranemreakgoz@gmail.com>
2022-02-28 15:24:37 +0300

    Building Spawn

--- Assets/Scripts/PiotSpawner.cs
+++ Assets/Scripts/PiotSpawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PiotSpawner : MonoBehaviour
 {
-    private int initamount = 5;
+    private int initamount = 2;
     private float pLotSize = 60f;
-    private float xPosleft = -20.7f;
-    private float xPosRight = 20.7f;
-    private float lastZpos = 15f;
+    private float xPosleft = -32.7f;
+    private float xPosRight = 32.7f;
+    private float lastZpos = 60f;
 
     public List<GameObject> Koseler;
     // Start is called before the first frame update
@@ -31,8 +31,8 @@ public class PiotSpawner : MonoBehaviour
         GameObject KoselerLeft = Koseler[Random.Range(0, Koseler.Count)];
         GameObject KoselerRight = Koseler[Random.Range(0, Koseler.Count)];
         float zPos = lastZpos + pLotSize;
-        Instantiate(KoselerLeft, new Vector3(xPosleft, 0.025f, zPos), KoselerLeft.transform.rotation);
-        Instantiate(KoselerRight, new Vector3(xPosRight, 0.025f, zPos), new Quaternion(0, 180, 0, 0));
+        Instantiate(KoselerLeft, new Vector3(xPosleft, 0, zPos), KoselerLeft.transform.rotation);
+        Instantiate(KoselerRight, new Vector3(xPosRight, 0, zPos), new Quaternion(0, 0, 0, 0));
         lastZpos += pLotSize;
     }
 }
--- Assets/Scripts/Spawnmanager.cs
+++ Assets/Scripts/Spawnmanager.cs
@@ -6,7 +6,7 @@ public class Spawnmanager : MonoBehaviour
 {
     RoadSpawner roadSpawner;
     PiotSpawner plotSpawner;
-    //test
+    
     // Start is called before the first frame update
     void Start()
     {
